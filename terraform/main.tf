c2ceffade4f43a85bd3d22d418cfc193a7dda9b8
jayanthpjh <58696269+jayanthpjh@users.noreply.github.com>
2025-06-05 16:09:27 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,3 @@ provider "aws" {
   region = var.region
 }
 
-module "s3" {
-  source = "./s3"
-}
-
-module "dynamodb" {
-  source = "./dynamodb"
-}
-
-module "lambda" {
-  source = "./lambda"
-}
-
-module "iam" {
-  source = "./iam"
-}
