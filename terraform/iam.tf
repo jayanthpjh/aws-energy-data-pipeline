7adf28f4b2da451a2400e4a4a0a6543051dcaf49
jayanthpjh <58696269+jayanthpjh@users.noreply.github.com>
2025-06-09 15:15:01 -0400

    Make IAM role names unique

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_exec" {
-  name = "energy_data_lambda_exec_role"
+  name = "energy_data_lambda_exec_role-${random_string.iam_suffix.result}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -15,7 +15,7 @@ resource "aws_iam_role" "lambda_exec" {
 }
 
 resource "aws_iam_policy_attachment" "lambda_logs" {
-  name       = "energy_data_lambda_logs"
+  name       = "energy_data_lambda_logs-${random_string.iam_suffix.result}"
   roles      = [aws_iam_role.lambda_exec.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
--- terraform/random.tf
+++ terraform/random.tf
@@ -5,6 +5,12 @@ resource "random_string" "s3_suffix" {
 }
 
 
+resource "random_string" "iam_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 resource "random_string" "dynamodb_suffix" {
   length  = 8
   special = false
