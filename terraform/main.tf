1334cc7bc64d818e7463cf31a7c9acbb764555f6
jayanthpjh <jayanthpjh@gmail.com>
2025-06-10 22:36:19 -0400

    test push 19

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,25 @@
-resource "random_pet" "suffix" {
-  length = 2
-}
-
-
 provider "aws" {
   region = var.region
+}
+
+resource "random_pet" "suffix" {}
+
+module "s3" {
+  source = "./s3"
+}
+
+module "dynamodb" {
+  source = "./dynamodb"
+}
+
+module "iam" {
+  source = "./iam"
+}
+
+module "lambda" {
+  source = "./lambda"
+}
+
+module "schedule" {
+  source = "./schedule"
 }
