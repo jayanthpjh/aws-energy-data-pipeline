9bd830da1fd993b830405d3d91d7121456ec122e
jayanthpjh <jayanthpjh@gmail.com>
2025-06-11 00:20:26 -0400

    push

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,7 +1,7 @@
 data "archive_file" "generate_data_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../data_generator/generate_data.zip"
-  output_path = "${path.module}/../data_generator/generate_data.zip"
+  source_dir  = "${path.module}/../data_generator"
+  output_path = "${path.module}/../data_generator.zip"
 }
 
 data "archive_file" "process_data_zip" {
