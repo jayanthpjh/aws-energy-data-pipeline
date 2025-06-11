a2591b9d51bc950906df2c79c9694da5c70c27e2
jayanthpjh <jayanthpjh@gmail.com>
2025-06-10 21:39:29 -0400

    test push 15

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,21 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "lambda_access_policy" {
   name        = "lambda-access-policy"
   description = "Allow Lambda to access S3 and DynamoDB"
@@ -6,9 +24,9 @@ resource "aws_iam_policy" "lambda_access_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid      = "S3PutAccess"
-        Effect   = "Allow"
-        Action   = [
+        Sid    = "S3PutAccess"
+        Effect = "Allow"
+        Action = [
           "s3:PutObject",
           "s3:GetObject",
           "s3:ListBucket"
@@ -19,9 +37,9 @@ resource "aws_iam_policy" "lambda_access_policy" {
         ]
       },
       {
-        Sid      = "DynamoDBAccess"
-        Effect   = "Allow"
-        Action   = [
+        Sid    = "DynamoDBAccess"
+        Effect = "Allow"
+        Action = [
           "dynamodb:PutItem",
           "dynamodb:UpdateItem",
           "dynamodb:GetItem"
@@ -29,13 +47,13 @@ resource "aws_iam_policy" "lambda_access_policy" {
         Resource = "arn:aws:dynamodb:*:*:table/Energy_Data_Info-*"
       },
       {
-        Sid    = "LogsAccess",
-        Effect = "Allow",
+        Sid    = "LogsAccess"
+        Effect = "Allow"
         Action = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents"
-        ],
+        ]
         Resource = "*"
       }
     ]
