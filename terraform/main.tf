0faa7a8fb270fadc3efa4e7aee6003b1b6948a2d
jayanthpjh <58696269+jayanthpjh@users.noreply.github.com>
2025-06-10 22:38:47 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,23 +3,3 @@ provider "aws" {
 }
 
 resource "random_pet" "suffix" {}
-
-module "s3" {
-  source = "./s3"
-}
-
-module "dynamodb" {
-  source = "./dynamodb"
-}
-
-module "iam" {
-  source = "./iam"
-}
-
-module "lambda" {
-  source = "./lambda"
-}
-
-module "schedule" {
-  source = "./schedule"
-}
