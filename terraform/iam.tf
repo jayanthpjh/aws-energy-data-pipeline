49c90793be0a45e8985d1fb6960b6681410691ce
jayanthpjh <jayanthpjh@gmail.com>
2025-06-10 22:42:45 -0400

    test 20

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+# Data Feeder IAM Role
 resource "aws_iam_role" "lambda_exec_data_feeder" {
   name = "lambda-exec-feeder-${random_pet.suffix.id}"
   assume_role_policy = jsonencode({
@@ -14,14 +15,31 @@ resource "aws_iam_policy" "data_feeder_policy" {
   name = "feeder-policy-${random_pet.suffix.id}"
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"],
-      Effect = "Allow",
-      Resource = [
-        "arn:aws:s3:::energy-data-*",
-        "arn:aws:s3:::energy-data-*/*"
-      ]
-    }]
+    Statement = [
+      {
+        Sid    = "S3Access",
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          "arn:aws:s3:::${var.s3_bucket_name}",
+          "arn:aws:s3:::${var.s3_bucket_name}/*"
+        ]
+      },
+      {
+        Sid    = "CloudWatchLogs",
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
   })
 }
 
@@ -30,7 +48,7 @@ resource "aws_iam_role_policy_attachment" "feeder_attach" {
   policy_arn = aws_iam_policy.data_feeder_policy.arn
 }
 
-# Processor Role
+# Processor IAM Role
 resource "aws_iam_role" "lambda_exec_processor" {
   name = "lambda-exec-processor-${random_pet.suffix.id}"
   assume_role_policy = jsonencode({
@@ -49,17 +67,36 @@ resource "aws_iam_policy" "processor_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = ["s3:GetObject", "s3:ListBucket"],
-        Effect = "Allow",
+        Sid    = "S3Access",
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Effect   = "Allow",
         Resource = [
-          "arn:aws:s3:::energy-data-*",
-          "arn:aws:s3:::energy-data-*/*"
+          "arn:aws:s3:::${var.s3_bucket_name}",
+          "arn:aws:s3:::${var.s3_bucket_name}/*"
         ]
       },
       {
-        Action = ["dynamodb:PutItem", "dynamodb:UpdateItem", "dynamodb:GetItem"],
+        Sid    = "DynamoDBAccess",
         Effect = "Allow",
-        Resource = "arn:aws:dynamodb:*:*:table/Energy_Data_Info-*"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:GetItem"
+        ],
+        Resource = "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/Energy_Data_Info-${random_pet.suffix.id}"
+      },
+      {
+        Sid    = "CloudWatchLogs",
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
       }
     ]
   })
